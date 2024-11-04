af3aa843878de81000fa78c5e5ccb979c06257bd
kajiki0 <l0wlevelbrain@gmail.com>
2024-11-04 16:43:10 -0300

    example 2

--- array.c
+++ array.c
@@ -36,25 +36,53 @@ Array:
 
 //example1:
 
-#include <stdio.h>
-int main(){
-    //array for integers
-    //index: 0 to (n-1), n=10 -> index: 0 to 9
-    int rollNo[10];
+// #include <stdio.h>
+// int main(){
+//     //array for integers
+//     //index: 0 to (n-1), n=10 -> index: 0 to 9
+//     int rollNo[10];
     
-    //integer inputs
-    for(int i=0;i<10;i++)
-        scanf("%d",&rollNo[i]);
+//     //integer inputs
+//     for(int i=0;i<10;i++)
+//         scanf("%d",&rollNo[i]);
 
-    //var i is index position 
-    //output
-    for(int i=0;i<10;i++)
-        printf("%d ",rollNo[i]);
+//     //var i is index position 
+//     //output
+//     for(int i=0;i<10;i++)
+//         printf("%d ",rollNo[i]);
 
 
+//     return 0;
+
+// }
+
+
+//example 2: average of int numbers
+#include <stdio.h>
+
+int main(){
+    int values[20];
+    int sum=0,average=0,n; //be careful with garbage values!
+
+    printf("enter the number of elements(max is 20): ");
+    scanf("%d",&n);
+
+    for(int i=0;i<n;++i){
+        //i+1 because the array syntax,otherwise its like " how many fingers we have?...yes,9 fingers"
+        printf("Enter number(%d): ",i+1);
+        scanf("%d",&values[i]);
+        sum += values[i];
+        //sum = 0
+        //sum = sum + values[0]
+        //sum = 0 + values[0]
+        //sum = values[0]
+        //sum = sum + values[1]
+        //sum = values[0] + values[1]
+        // ...  
+
+    }
+
+    average = sum/n; //arithmetic mean = sum of values/number of values
+    printf("Average = %d", average);
     return 0;
-
 }
-
-
-//example 2:
